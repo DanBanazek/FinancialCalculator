e82c0ac9df4a7597b0a0b394be5156bd7fef0c5f
Dan Banazek <dan.banazek@gmail.com>
2016-10-06 21:44:27 -0500

    Added Project Files

---
+++ calculator/finCalc/Scripts/viewScripts/index.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#startVal').kendoNumericTextBox({
+        format: "c",
+        decimals: 3,
+        step: 100,
+        spinners: false
+    });
+    $('#intRate').kendoNumericTextBox({
+        format: "p0",
+        min: 0,
+        max: 1,
+        step: 0.025
+    });
+    $('#term').kendoNumericTextBox({
+        step: .5
+    });
+    $('#calcReturn').kendoButton();
+});
---
+++ calculator/finCalc/app/main.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main($uibModal) {
+        var vm = this;
+        //vm.food = 'pizza';
+        vm.animationsEnabled = true;
+        vm.open = function (size) {
+            var modalInstance = $uibModal.open({
+                animation: vm.animationsEnabled,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'Partials/NewAssetForm.html',
+                controller:'ModalInstanceCtrl',
+                controllerAs: 'vm',
+                size: size
+            });
+        }
+        //vm.close = function ()
+        //{
+        //    vm.dismiss({ $value: 'cancel' });
+        //};
+    }
+
+
+})();
+
+angular.module('app').controller('ModalInstanceCtrl', function ($uibModalInstance) {
+    var $ctrl = this;
+    //$ctrl.items = items;
+    //$ctrl.selected = {
+    //    item: $ctrl.items[0]
+    //};
+
+    $ctrl.ok = function () {
+        $uibModalInstance.close($ctrl.selected.item);
+    };
+
+    $ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
